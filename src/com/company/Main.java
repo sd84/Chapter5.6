c28dd7083d873f6bf56fe7a4ee227b540fe4c264
stanislavderkach <dsv_1984@ukr.net>
2022-01-06 00:08:48 +0200

    added TreeMap class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        TreeMapClass obj1 = new TreeMapClass();
+        obj1.map_populate();
+        obj1.get_keys();
     }
 }
